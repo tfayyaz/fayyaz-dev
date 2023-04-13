e81439a6e1b0d941ffcdf1dbfa302115f29c6901
tfayyaz <tahir.fayyaz@databricks.com>
2023-04-13 08:17:04 +0100

    Moved blog to posts directory

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'fayyaz.dev';
+export const SITE_DESCRIPTION = 'Ideas on data, LLMs and app development';
